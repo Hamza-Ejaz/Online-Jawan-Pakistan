6167f310a683e6a654ca793c5c9a7f0cd2dc1284
Hamza-Ejaz <81479080+Hamza-Ejaz@users.noreply.github.com>
2021-05-08 22:53:15 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Page());
+}
+
+class Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("HAMZA EJAZ"),
+      ),
+    );
+  }
+}
